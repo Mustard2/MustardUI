3c05b6abfdc3b4afedfb91ba1b1fecf174226c45
Mustard <mustard_sfm@protonmail.com>
2023-11-14 20:34:17 +0100

    Temporary fix for #130

--- settings/addon_prefs.py
+++ settings/addon_prefs.py
@@ -57,7 +57,12 @@ class MustardUI_AddonPrefs(bpy.types.AddonPreferences):
 
 
 def register():
-    bpy.utils.register_class(MustardUI_AddonPrefs)
+    # Fix for bug #130 (temporary solution)
+    try:
+        bpy.utils.register_class(MustardUI_AddonPrefs)
+    except:
+        bpy.utils.unregister_class(MustardUI_AddonPrefs)
+        bpy.utils.register_class(MustardUI_AddonPrefs)
 
 
 def unregister():
