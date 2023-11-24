afb2f248df94531524dfd53e085233092c46dc20
Mustard <mustard_sfm@protonmail.com>
2023-11-24 15:43:09 +0100

    Added eevee optimized normals in cycles warning

--- menu/menu_warnings.py
+++ menu/menu_warnings.py
@@ -1,6 +1,7 @@
 import bpy
 from . import MainPanel
 from ..warnings.ops_fix_old_UI import check_old_UI
+from ..warnings.ops_fix_eevee_normals import check_eevee_normals
 
 
 class PANEL_PT_MustardUI_Warnings(MainPanel, bpy.types.Panel):
@@ -11,12 +12,15 @@ class PANEL_PT_MustardUI_Warnings(MainPanel, bpy.types.Panel):
     @classmethod
     def poll(cls, context):
         # Check presence of old UI scripts
-        return check_old_UI()
+        settings = bpy.context.scene.MustardUI_Settings
+        return check_old_UI() or check_eevee_normals(context.scene, settings)
 
     def draw_header(self, context):
         self.layout.label(text="", icon="ERROR")
 
     def draw(self, context):
+
+        settings = bpy.context.scene.MustardUI_Settings
 
         layout = self.layout
 
@@ -24,9 +28,14 @@ class PANEL_PT_MustardUI_Warnings(MainPanel, bpy.types.Panel):
         if check_old_UI():
             box = layout.box()
             col = box.column(align=True)
-            col.label(text="Old UI script found", icon="ERROR")
-            col.label(text="Remove the scripts to avoid errors", icon="BLANK1")
+            col.label(text="Old UI script found!", icon="ERROR")
             box.operator("mustardui.warnings_fix_old_ui")
+
+        if check_eevee_normals(context.scene, settings):
+            box = layout.box()
+            col = box.column(align=True)
+            col.label(text="Eevee Optimed Normals are active with Cycles!", icon="ERROR")
+            box.operator("mustardui.warnings_fix_eevee_normals")
 
 
 def register():
