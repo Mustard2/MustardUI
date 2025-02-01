8cf4503a65af8477f0644059a2885d6397fcfab1
Mustard <mustard_sfm@protonmail.com>
2025-02-01 20:08:48 +0100

    Removed unused message_box

--- misc/message_box.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-
-def ShowMessageBox(message="", title="Message Box", icon='INFO'):
-    def draw(self, context):
-        self.layout.label(text=message)
-
-    bpy.context.window_manager.popup_menu(draw, title=title, icon=icon)
