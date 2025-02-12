0d79a82a5f18df91618059055d23a047de1cadb4
Mustard <mustard_sfm@protonmail.com>
2025-02-12 03:14:41 +0100

    Tools additions

--- menu/menu_tools_creators.py
+++ menu/menu_tools_creators.py
@@ -19,35 +19,80 @@ class PANEL_PT_MustardUI_ToolsCreators(MainPanel, bpy.types.Panel):
         return res and addon_prefs.developer
 
     def draw(self, context):
+        pass
+
+
+class PANEL_PT_MustardUI_ToolsCreators_Physics(MainPanel, bpy.types.Panel):
+    bl_parent_id = "PANEL_PT_MustardUI_ToolsCreators"
+    bl_label = "Physics"
+    bl_options = {"DEFAULT_CLOSED"}
+
+    @classmethod
+    def poll(cls, context):
+        if check_old_UI():
+            return False
+
+        res, arm = mustardui_active_object(context, config=1)
+        addon_prefs = context.preferences.addons[base_package].preferences
+        return res and addon_prefs.developer
+
+    def draw_header(self, context):
+        layout = self.layout
+        layout.label(text="", icon="PHYSICS")
+
+    def draw(self, context):
 
         layout = self.layout
 
-        box = layout.box()
-        box.label(text="Physics", icon="PHYSICS")
-        row = box.row(align=True)
+        row = layout.row(align=True)
         row.operator('mustardui.tools_creators_create_jiggle', text="Add Jiggle Cage", icon="OUTLINER_OB_FORCE_FIELD")
         row.operator('mustardui.tools_creators_bone_physics_clean', text="", icon="X")
-        row = box.row(align=True)
+        row = layout.row(align=True)
         row.operator('mustardui.tools_creators_bone_physics', text="Add Bone Physics", icon="BONE_DATA")
-        row = box.row(align=True)
+        row = layout.row(align=True)
         row.operator('mustardui.tools_creators_hair_cage', text="Create Hair Cage", icon="OUTLINER_OB_CURVES")
         row.operator('mustardui.tools_creators_add_cloth_to_hair', text="", icon="MOD_CLOTH")
-        row = box.row(align=True)
+        row = layout.row(align=True)
         row.operator('mustardui.tools_creators_create_collision_cage', text="Create Collision Cage", icon="MESH_UVSPHERE")
 
-        box = layout.box()
-        box.label(text="Rig", icon="OUTLINER_DATA_ARMATURE")
-        row = box.row(align=True)
+
+class PANEL_PT_MustardUI_ToolsCreators_Rig(MainPanel, bpy.types.Panel):
+    bl_parent_id = "PANEL_PT_MustardUI_ToolsCreators"
+    bl_label = "Armature"
+    bl_options = {"DEFAULT_CLOSED"}
+
+    @classmethod
+    def poll(cls, context):
+        if check_old_UI():
+            return False
+
+        res, arm = mustardui_active_object(context, config=1)
+        addon_prefs = context.preferences.addons[base_package].preferences
+        return res and addon_prefs.developer
+
+    def draw_header(self, context):
+        layout = self.layout
+        layout.label(text="", icon="OUTLINER_DATA_ARMATURE")
+
+    def draw(self, context):
+
+        layout = self.layout
+
+        row = layout.row(align=True)
         row.operator('mustardui.tools_creators_ikspline', text="Create IK Spline", icon="CON_SPLINEIK")
         row.operator('mustardui.tools_creators_ikspline_clean', text="", icon="X")
-        row = box.row(align=True)
+        row = layout.row(align=True)
         row.operator('mustardui.tools_creators_affect_transform', text="Affect Transform on Bone Constraints", icon="CONSTRAINT_BONE").enable = True
         row.operator('mustardui.tools_creators_affect_transform', text="", icon="X").enable = False
 
 
 def register():
     bpy.utils.register_class(PANEL_PT_MustardUI_ToolsCreators)
+    bpy.utils.register_class(PANEL_PT_MustardUI_ToolsCreators_Physics)
+    bpy.utils.register_class(PANEL_PT_MustardUI_ToolsCreators_Rig)
 
 
 def unregister():
+    bpy.utils.unregister_class(PANEL_PT_MustardUI_ToolsCreators_Rig)
+    bpy.utils.unregister_class(PANEL_PT_MustardUI_ToolsCreators_Physics)
     bpy.utils.unregister_class(PANEL_PT_MustardUI_ToolsCreators)
