e191fc0b46c20fb32fe89ec6b5200287ba6ea1ec
Mustard <mustard_sfm@protonmail.com>
2025-04-26 13:10:07 +0200

    Fix for eevee normals tool

--- tools/eevee_normals.py
+++ tools/eevee_normals.py
@@ -223,7 +223,7 @@ def default_custom_nodes():
     node.inputs[0].default_value = 1.0  # Strength
     node.inputs[1].default_value = 1000.0  # Distance
     node.inputs[2].default_value = 1.0  # Height
-    node.inputs[3].default_value = (0.0, 0.0, 0.0)  # Normal
+    node.inputs[4].default_value = (0.0, 0.0, 0.0)  # Normal
 
     node = nodes.new('ShaderNodeBump')
     node.name = 'Tangent'
