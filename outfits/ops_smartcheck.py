2a231ce56304768fa66e7a642fc97c4412fa0213
Mustard <mustard_sfm@protonmail.com>
2025-05-03 15:49:01 +0200

    Fix

--- outfits/ops_smartcheck.py
+++ outfits/ops_smartcheck.py
@@ -31,10 +31,10 @@ class MustardUI_Outfit_SmartCheck(bpy.types.Operator):
             print('\nMustardUI - Smart Check - Searching for outfits\n')
 
         outfits_collections = [x for x in bpy.data.collections if
-                               (rig_settings.model_name in x.name) and (not 'Hair' in x.name) and
-                               (not 'Extras' in x.name) and (not 'Physics' in x.name) and
-                               (not 'Collision' in x.name) and
-                               (not rig_settings.model_name == x.name) and (not '_' in x.name)]
+                               (rig_settings.model_name in x.name) and ('Hair' not in x.name) and
+                               ('Extras' not in x.name) and ('Physics' not in x.name) and
+                               ('Collision' not in x.name) and
+                               (rig_settings.model_name != x.name) and ('_' not in x.name)]
 
         for collection in outfits_collections:
 
