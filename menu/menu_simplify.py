53a15a8306577566972a788165d9a5b9849bc1cb
Mustard <mustard_sfm@protonmail.com>
2026-03-07 23:52:58 +0100

    Initial refactor of Simplify function

--- menu/menu_simplify.py
+++ menu/menu_simplify.py
@@ -18,29 +18,29 @@ class PANEL_PT_MustardUI_Simplify(MainPanel, bpy.types.Panel):
 
         res, arm = mustardui_active_object(context, config=0)
 
-        if arm is not None:
-            rig_settings = arm.MustardUI_RigSettings
-            if hasattr(rig_settings, "simplify_main_enable"):
-                return res and rig_settings.simplify_main_enable
-            else:
-                return False
+        if arm is None:
+            return False
 
-        return res
+        simplify_settings = context.scene.MustardUI_SimplifySettings
+        if hasattr(simplify_settings, "simplify_main_enable"):
+            return res and simplify_settings.simplify_main_enable
+        else:
+            return False
 
     def draw_header(self, context):
-
-        poll, obj = mustardui_active_object(context, config=0)
-        rig_settings = obj.MustardUI_RigSettings
-
-        self.layout.prop(rig_settings, "simplify_enable", text="", toggle=False)
+        simplify_settings = context.scene.MustardUI_SimplifySettings
+        self.layout.prop(simplify_settings, "simplify_enable", text="", toggle=False)
 
     def draw(self, context):
 
-        settings = bpy.context.scene.MustardUI_Settings
+        settings = context.scene.MustardUI_Settings
         poll, obj = mustardui_active_object(context, config=0)
+
         rig_settings = obj.MustardUI_RigSettings
         morphs_settings = obj.MustardUI_MorphsSettings
         physics_settings = obj.MustardUI_PhysicsSettings
+
+        simplify_settings = context.scene.MustardUI_SimplifySettings
 
         layout = self.layout
         layout.enabled = not rig_settings.simplify_enable
@@ -49,42 +49,42 @@ class PANEL_PT_MustardUI_Simplify(MainPanel, bpy.types.Panel):
         box.label(text="General", icon="OPTIONS")
         col = box.column(align=True)
         row = col.row()
-        row.prop(rig_settings, "simplify_blender")
+        row.prop(simplify_settings, "simplify_blender")
         row.scale_x = 0.5
         col2 = row.column()
         col2.enabled = rig_settings.simplify_blender
         col2.prop(context.scene.render, "simplify_subdivision", text="Max Subdiv")
         if rig_settings.outfits_enable_global_subsurface or rig_settings.body_enable_subdiv:
-            col.prop(rig_settings, "simplify_subdiv")
+            col.prop(simplify_settings, "simplify_subdiv")
         if rig_settings.outfits_enable_global_normalautosmooth or rig_settings.body_enable_norm_autosmooth:
-            col.prop(rig_settings, "simplify_normals_autosmooth")
-        col.prop(rig_settings, "simplify_normals_optimize")
+            col.prop(simplify_settings, "simplify_normals_autosmooth")
+        col.prop(simplify_settings, "simplify_normals_optimize")
 
         box = layout.box()
         box.label(text="Objects", icon="OUTLINER_OB_ARMATURE")
         col = box.column(align=True)
         if morphs_settings.enable_ui:
-            col.prop(rig_settings, "simplify_diffeomorphic")
+            col.prop(simplify_settings, "simplify_diffeomorphic")
         if rig_settings.outfit_nude:
-            col.prop(rig_settings, "simplify_outfit_switch_nude")
-        col.prop(rig_settings, "simplify_outfit_global")
-        col.prop(rig_settings, "simplify_extras")
-        col.prop(rig_settings, "simplify_armature_child")
+            col.prop(simplify_settings, "simplify_outfit_switch_nude")
+        col.prop(simplify_settings, "simplify_outfit_global")
+        col.prop(simplify_settings, "simplify_extras")
+        col.prop(simplify_settings, "simplify_armature_child")
         col.separator()
-        col.prop(rig_settings, "simplify_hair")
-        col.prop(rig_settings, "simplify_hair_global")
-        col.prop(rig_settings, "simplify_particles")
+        col.prop(simplify_settings, "simplify_hair")
+        col.prop(simplify_settings, "simplify_hair_global")
+        col.prop(simplify_settings, "simplify_particles")
 
         if len(physics_settings.items) > 0:
             col.separator()
-            col.prop(rig_settings, "simplify_physics")
+            col.prop(simplify_settings, "simplify_physics")
 
         if settings.advanced:
             box = layout.box()
             box.label(text="Global Disable", icon="WORLD")
             col = box.column(align=True)
-            col.prop(rig_settings, "simplify_force_no_physics")
-            col.prop(rig_settings, "simplify_force_no_particles")
+            col.prop(simplify_settings, "simplify_force_no_physics")
+            col.prop(simplify_settings, "simplify_force_no_particles")
 
 
 def register():
