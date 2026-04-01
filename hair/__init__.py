4312f35fd5df2a7a74a0b7877fde862428d15934
Mustard <mustard_sfm@protonmail.com>
2026-04-01 20:47:35 +0200

    Fixes

--- hair/__init__.py
+++ hair/__init__.py
@@ -1,12 +1,15 @@
 from . import ops_hair_visibility
 from . import ops_optimize
+from . import ops_hair_particles
 
 
 def register():
     ops_hair_visibility.register()
+    ops_hair_particles.register()
     ops_optimize.register()
 
 
 def unregister():
     ops_optimize.unregister()
+    ops_hair_particles.unregister()
     ops_hair_visibility.unregister()
