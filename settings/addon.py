b7ec9df0eae6ec9b60a40011f29e5324dbee0b1f
Ayoub ibn Muhammad <cl3m3c7@riseup.net>
2026-04-02 08:24:07 +0100

    Format: settings package

--- settings/addon.py
+++ settings/addon.py
@@ -1,6 +1,7 @@
 import bpy
-from bpy.props import *
 from bpy.app.handlers import persistent
+from bpy.props import BoolProperty, CollectionProperty, PointerProperty, StringProperty
+
 from .. import __package__ as base_package
 from ..outfits.ops_rename_outfit import MustardUI_RenameOutfit_Class
 
@@ -8,45 +9,59 @@ from ..outfits.ops_rename_outfit import MustardUI_RenameOutfit_Class
 class MustardUI_Settings(bpy.types.PropertyGroup):
     # Main Settings definitions
     # Version of the MustardUI
-    version: StringProperty(default="0.0.1",
-                            name="MustardUI version",
-                            description="Version of MustardUI add-on")
+    version: StringProperty(
+        default="0.0.1",
+        name="MustardUI version",
+        description="Version of MustardUI add-on",
+    )
 
     # Advanced settings
-    advanced: BoolProperty(default=False,
-                           name="Advanced Options",
-                           description="Unlock Advanced Options.\nMore advanced options will be shown in "
-                                       "the UI")
+    advanced: BoolProperty(
+        default=False,
+        name="Advanced Options",
+        description="Unlock Advanced Options.\nMore advanced options will be shown in "
+        "the UI",
+    )
 
     # Model selection
-    viewport_model_selection: BoolProperty(default=True,
-                                           name="Viewport model selection",
-                                           description="Enable viewport model selection.\nIf disabled, "
-                                                       "a model selection panel will appear instead, "
-                                                       "where a model can be selected")
-    viewport_model_selection_after_configuration: BoolProperty(default=False,
-                                                               name="Viewport model selection",
-                                                               description="Enable viewport model selection "
-                                                                           "after the end of the "
-                                                                           "configuration.\nIt is advised "
-                                                                           "to leave this off if only one "
-                                                                           "character with MustardUI is "
-                                                                           "available on the scene")
+    viewport_model_selection: BoolProperty(
+        default=True,
+        name="Viewport model selection",
+        description="Enable viewport model selection.\nIf disabled, "
+        "a model selection panel will appear instead, "
+        "where a model can be selected",
+    )
+    viewport_model_selection_after_configuration: BoolProperty(
+        default=False,
+        name="Viewport model selection",
+        description="Enable viewport model selection "
+        "after the end of the "
+        "configuration.\nIt is advised "
+        "to leave this off if only one "
+        "character with MustardUI is "
+        "available on the scene",
+    )
 
     panel_model_selection_armature: PointerProperty(type=bpy.types.Armature)
 
     # Property for custom properties errors
-    custom_properties_error: BoolProperty(name="",
-                                          description="Can not find the property.\nCheck the property or use the "
-                                                      "Re-build Custom Properties operator in Settings")
-    custom_properties_error_nonanimatable: BoolProperty(name="",
-                                                        description="Can not find the property.\nRemove the property "
-                                                                    "in the Configuration panel and add it again")
+    custom_properties_error: BoolProperty(
+        name="",
+        description="Can not find the property.\nCheck the property or use the "
+        "Re-build Custom Properties operator in Settings",
+    )
+    custom_properties_error_nonanimatable: BoolProperty(
+        name="",
+        description="Can not find the property.\nRemove the property "
+        "in the Configuration panel and add it again",
+    )
 
     # Property for morphs errors
-    daz_morphs_error: BoolProperty(name="",
-                                   description="Can not find the Daz Morph.\nRe-run the Check Morphs operator in the "
-                                               "Configuration menu to solve this")
+    daz_morphs_error: BoolProperty(
+        name="",
+        description="Can not find the Daz Morph.\nRe-run the Check Morphs operator "
+        "in the Configuration menu to solve this",
+    )
 
     # Material normals mute
     def update_material_normal(self, context):
@@ -55,36 +70,42 @@ class MustardUI_Settings(bpy.types.PropertyGroup):
 
         return
 
-    material_normal_nodes: BoolProperty(default=False,
-                                        name="Eeevee Optimized Normals",
-                                        description="Enable an optimized version of Eevee normals.\nThis tool "
-                                                    "substitutes normal nodes with more efficient ones, which can be "
-                                                    "useful to get better performance in Render Viewport mode. "
-                                                    "Disable this for Cycles",
-                                        update=update_material_normal)
+    material_normal_nodes: BoolProperty(
+        default=False,
+        name="EEVEE Optimized Normals",
+        description="Enable an optimized version of Eevee normals.\nThis tool "
+        "substitutes normal nodes with more efficient ones, which can be "
+        "useful to get better performance in Render Viewport mode. "
+        "Disable this for Cycles",
+        update=update_material_normal,
+    )
 
     # Tools (internal)
     # Class to store temporary settings for outfit rename
-    rename_outfits_temp_class: CollectionProperty(
-            type=MustardUI_RenameOutfit_Class)
+    rename_outfits_temp_class: CollectionProperty(type=MustardUI_RenameOutfit_Class)
 
 
-# Handler to solve a missing UI when: developer mode is on, but the UI has been left in configuration mode
+# Handler to solve a missing UI when: developer mode is on, but the UI has been left
+# in configuration mode
 @persistent
 def load_handler(_):
     context = bpy.context
     scene = context.scene
     addon_prefs = context.preferences.addons[base_package].preferences
     if addon_prefs.debug:
-        print('MustardUI - Checking for missing UIs')
-    if hasattr(scene, 'MustardUI_Settings'):
+        print("MustardUI - Checking for missing UIs")
+    if hasattr(scene, "MustardUI_Settings"):
         for obj in [x for x in scene.objects if x.type == "ARMATURE"]:
             arm = obj.data
-            if hasattr(arm, 'MustardUI_created') and hasattr(arm, 'MustardUI_enable'):
-                if arm.MustardUI_created and not arm.MustardUI_enable and not addon_prefs.developer:
+            if hasattr(arm, "MustardUI_created") and hasattr(arm, "MustardUI_enable"):
+                if (
+                    arm.MustardUI_created
+                    and not arm.MustardUI_enable
+                    and not addon_prefs.developer
+                ):
                     arm.MustardUI_enable = not arm.MustardUI_enable
                     if addon_prefs.debug:
-                        print(f'MustardUI - Fixed missing UI on {repr(obj.name)}')
+                        print(f"MustardUI - Fixed missing UI on {repr(obj.name)}")
 
 
 def register():
@@ -93,12 +114,12 @@ def register():
     bpy.types.Scene.MustardUI_Settings = PointerProperty(type=MustardUI_Settings)
 
     # Properties to specify if the Armature has a MustardUI currently in use
-    bpy.types.Armature.MustardUI_enable = bpy.props.BoolProperty(default=False,
-                                                                 name="")
+    bpy.types.Armature.MustardUI_enable = bpy.props.BoolProperty(default=False, name="")
 
     # Properties to specify if the Armature has a MustardUI created
-    bpy.types.Armature.MustardUI_created = bpy.props.BoolProperty(default=False,
-                                                                  name="")
+    bpy.types.Armature.MustardUI_created = bpy.props.BoolProperty(
+        default=False, name=""
+    )
 
     bpy.app.handlers.load_post.append(load_handler)
 
