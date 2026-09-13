159efe18f9ac5c01099888e09ec541898a29689f
Mustard <mustard_sfm@protonmail.com>
2026-09-13 14:44:42 +0200

    fix: Low-resolution Jiggle Cage ignores Parent to Model without Add to Physics Panel

--- tools_creators/ops_jiggle.py
+++ tools_creators/ops_jiggle.py
@@ -723,16 +723,18 @@ class MustardUI_ToolsCreators_CreateJiggle(bpy.types.Operator):
             # Switch back to the previous mode (typically Object Mode)
             bpy.ops.object.mode_set(mode=current_mode)
 
-        # Add the object to the Physics Panel
-        if self.add_to_panel:
-            for obj in bpy.context.selected_objects:
+        for obj in bpy.context.selected_objects:
+            # Add the object to the Physics Panel
+            if self.add_to_panel:
                 add_item = physics_settings.items.add()
                 add_item.object = obj
                 add_item.type = "CAGE"
-                if self.parent_to_model and rig_settings.model_armature_object is not None:
-                    parent = rig_settings.model_armature_object
-                    obj.parent = parent
-                    obj.matrix_parent_inverse = parent.matrix_world.inverted()
+
+            # Parent the object to the Model Armature
+            if self.parent_to_model and rig_settings.model_armature_object is not None:
+                parent = rig_settings.model_armature_object
+                obj.parent = parent
+                obj.matrix_parent_inverse = parent.matrix_world.inverted()
 
         # Disable shadows for viewport/render
         for obj in bpy.context.selected_objects:
